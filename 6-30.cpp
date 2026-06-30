2129fe610b34b2069e618607b7c171e9d4c7cefe
Xu jiangtao <77684124+XxJjTt6@users.noreply.github.com>
2026-06-30 23:13:11 +0800

    Update 6-30.cpp

--- 6-30.cpp
+++ 6-30.cpp
@@ -26,3 +26,31 @@ public:
         return res;
     }
 };
+
+//x2，还是不会
+class Solution {
+public:
+    int minDistance(string word1, string word2) 
+    {
+        word1=" "+word1;
+        word2=" "+word2;
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> f(n+1,vector<int>(m+1));
+
+        for(int i=0;i<=m-1;i++) f[0][i]=i;
+        for(int i=0;i<=n-1;i++) f[i][0]=i;
+
+        for(int i=1;i<=n-1;i++)
+        {
+            for(int j=1;j<=m-1;j++)
+            {
+                f[i][j]=min(f[i-1][j],f[i][j-1])+1;
+                if(word1[i]==word2[j]) f[i][j]=min(f[i][j],f[i-1][j-1]);
+                else f[i][j]=min(f[i][j],f[i-1][j-1]+1);
+            }
+        }
+
+        return f[n-1][m-1];
+    }   
+};
